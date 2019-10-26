a3356d2dc4d3486dc090f0fbfcb58a4977605b69
modestasjurcius <modesjur@gmail.com>
2019-10-26 14:30:54 +0300

    added CComment class.

---
+++ src/ProjectData/CComment.java
@@ -0,0 +1,58 @@
+/**
+ * @author Modestas
+ */
+package ProjectData;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class CComment
+{
+    private ArrayList attachedFilesPaths;
+    private String comment;
+    
+    private Date dateOfComment;
+    private String dateString;
+    
+    public CComment(String comment)
+    {
+        this.comment = comment;
+        
+        Calendar calendar = Calendar.getInstance();
+        this.dateOfComment = calendar.getTime();
+        
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.dateString = formatter.format(this.dateOfComment); 
+    }
+    
+    public void editComment(String newComment)
+    {
+        this.comment = newComment;
+    }
+    
+    public boolean attachFile(String path)
+    {
+        if(this.attachedFilesPaths == null)
+        {
+            this.attachedFilesPaths = new ArrayList();
+        }
+        
+        this.attachedFilesPaths.add(path);
+        
+        return true;
+    }
+    
+    public boolean removeFile(String path)
+    {
+        if(this.attachedFilesPaths.contains(path))
+        {
+            this.attachedFilesPaths.remove(path);
+            return true;
+        }
+        return false;
+    }
+    
+}
