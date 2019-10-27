2eb04be886c99bc6fa2dd75855a82062f5286930
modestasjurcius <modesjur@gmail.com>
2019-10-28 00:21:43 +0200

    implemented project importer dialog

---
+++ src/ValdymoSistema/Views/ProjectImporterDialogController.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ValdymoSistema.Views;
+
+
+import ValdymoSistema.CEventHandler;
+import ValdymoSistema.Main;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.json.simple.parser.ParseException;
+
+/**
+ * FXML Controller class
+ *
+ * @author Modestas
+ */
+public class ProjectImporterDialogController implements Initializable
+{
+    private CEventHandler eventHandler;
+    @FXML
+    private ListView<String> savedProjectsListView;
+    private String selectedProjectName;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        this.eventHandler = Main.getEventHandler();
+        this.savedProjectsListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            
+        @Override
+        public void handle(MouseEvent event) {
+            selectedProjectName = savedProjectsListView.getSelectionModel().getSelectedItem();
+            
+            try
+            {
+                eventHandler.importProject(selectedProjectName);
+            } catch (IOException ex)
+            {
+                Logger.getLogger(ProjectImporterDialogController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (ParseException ex)
+            {
+                Logger.getLogger(ProjectImporterDialogController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            eventHandler.handleInfo(CEventHandler.eInfoType.INFO_PROJECT_IMPORTED, selectedProjectName);
+            closeProjectImporter();
+            
+            return;
+        }
+    });
+    } 
+    
+    public String getSelectedProjectName()
+    {
+        return this.selectedProjectName;
+    }
+    
+    public void setSavedProjectList(ObservableList<String> list)
+    {
+        this.savedProjectsListView.setItems(list);
+    }
+    
+    private void closeProjectImporter()
+    {
+        Stage stage = (Stage) this.savedProjectsListView.getScene().getWindow();
+        stage.close();
+    }
+}
