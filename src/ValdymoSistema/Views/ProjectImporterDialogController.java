62fd00e706344edeee2020bcd531371e6c8fd85e
modestasjurcius <modesjur@gmail.com>
2019-11-17 15:17:02 +0200

    ProjectImporterDialogController: projects are imported on double click, formatted code.

--- src/ValdymoSistema/Views/ProjectImporterDialogController.java
+++ src/ValdymoSistema/Views/ProjectImporterDialogController.java
@@ -1,10 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * FXML Controller class
+ *
+ * @author Modestas
  */
 package ValdymoSistema.Views;
-
 
 import ValdymoSistema.CEventHandler;
 import ValdymoSistema.Main;
@@ -14,23 +13,18 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import javafx.util.Pair;
 import org.json.simple.parser.ParseException;
 
-/**
- * FXML Controller class
- *
- * @author Modestas
- */
+
 public class ProjectImporterDialogController implements Initializable
 {
+
     private CEventHandler eventHandler;
     @FXML
     private ListView<String> savedProjectsListView;
@@ -40,44 +34,49 @@ public class ProjectImporterDialogController implements Initializable
     public void initialize(URL url, ResourceBundle rb)
     {
         this.eventHandler = Main.getEventHandler();
-        this.savedProjectsListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            
-        @Override
-        public void handle(MouseEvent event) {
-            selectedProjectName = savedProjectsListView.getSelectionModel().getSelectedItem();
-            
-            try
+        this.savedProjectsListView.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+
+            @Override
+            public void handle(MouseEvent event)
             {
-                eventHandler.importProject(selectedProjectName);
-            } catch (IOException ex)
-            {
-                Logger.getLogger(ProjectImporterDialogController.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (ParseException ex)
-            {
-                Logger.getLogger(ProjectImporterDialogController.class.getName()).log(Level.SEVERE, null, ex);
+                if (event.getClickCount() == 2)
+                {
+                    selectedProjectName = savedProjectsListView.getSelectionModel().getSelectedItem();
+
+                    try
+                    {
+                        eventHandler.importProject(selectedProjectName);
+                    } catch (IOException ex)
+                    {
+                        Logger.getLogger(ProjectImporterDialogController.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (ParseException ex)
+                    {
+                        Logger.getLogger(ProjectImporterDialogController.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    eventHandler.handleInfo(CEventHandler.eInfoType.INFO_PROJECT_IMPORTED, selectedProjectName);
+                    closeProjectImporter();
+
+                    return;
+                }
             }
-            
-            eventHandler.handleInfo(CEventHandler.eInfoType.INFO_PROJECT_IMPORTED, selectedProjectName);
-            closeProjectImporter();
-            
-            return;
-        }
-    });
-    } 
-    
+        });
+    }
+
     public String getSelectedProjectName()
     {
         return this.selectedProjectName;
     }
-    
+
     public void setSavedProjectList(Map map)
     {
-        for(Object name : map.keySet())
+        for (Object name : map.keySet())
         {
-            this.savedProjectsListView.getItems().add((String)name);
+            this.savedProjectsListView.getItems().add((String) name);
         }
     }
-    
+
     private void closeProjectImporter()
     {
         Stage stage = (Stage) this.savedProjectsListView.getScene().getWindow();
