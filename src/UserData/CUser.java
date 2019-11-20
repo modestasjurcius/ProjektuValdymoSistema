696afbacdb29546d8e6477cfe176d340b5eb69b7
modestasjurcius <modesjur@gmail.com>
2019-11-20 21:40:17 +0200

    CUser: id getter

--- src/UserData/CUser.java
+++ src/UserData/CUser.java
@@ -1,22 +1,27 @@
 /**
  * @author Modestas
  */
-
 package UserData;
 
 public class CUser
 {
+
     private String userName;
     private int id;
-    
+
     public CUser(String name, int id)
     {
         this.userName = name;
         this.id = id;
     }
-    
+
     public String getUserName()
     {
         return this.userName;
+    }
+
+    public int getId()
+    {
+        return this.id;
     }
 }
