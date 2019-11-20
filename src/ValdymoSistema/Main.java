d5f83bda66a8e783401d55c32254488d0f2be34d
modestasjurcius <modesjur@gmail.com>
2019-11-20 21:44:39 +0200

    Main: removed comment

--- src/ValdymoSistema/Main.java
+++ src/ValdymoSistema/Main.java
@@ -39,7 +39,6 @@ public class Main extends Application
         Scene scene = new Scene(root);
 
         primaryStage.setScene(scene);
-        //primaryStage.show();
 
         mainController = loader.<MainController>getController();
 
