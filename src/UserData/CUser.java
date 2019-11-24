bc1562cfb50d74f73ef5e7a6e59e2978cca9b027
modestasjurcius <modesjur@gmail.com>
2019-11-24 22:08:20 +0200

    CUser: added more data for user class.

--- src/UserData/CUser.java
+++ src/UserData/CUser.java
@@ -7,12 +7,18 @@ public class CUser
 {
 
     private String userName;
+    private String fullUserName;
+    private String userContacts;
+    private boolean isSingleUser;
     private int id;
 
-    public CUser(String name, int id)
+    public CUser(String name, int id, boolean isSingleUser, String fullName, String contacts)
     {
         this.userName = name;
         this.id = id;
+        this.isSingleUser = isSingleUser;
+        this.fullUserName = fullName;
+        this.userContacts = contacts;
     }
 
     public String getUserName()
@@ -23,5 +29,20 @@ public class CUser
     public int getId()
     {
         return this.id;
+    }
+    
+    public String getUserFullName()
+    {
+        return this.fullUserName;
+    }
+    
+    public String getUserContacts()
+    {
+        return this.userContacts;
+    }
+    
+    public boolean isSingleUser()
+    {
+        return this.isSingleUser;
     }
 }
