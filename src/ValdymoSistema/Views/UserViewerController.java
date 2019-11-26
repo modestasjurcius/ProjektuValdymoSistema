218d3bc7a523ad747244a3e6e38f5e9f73530ff1
modestasjurcius <modesjur@gmail.com>
2019-11-27 00:14:39 +0200

    UserViewerController: editor/view mode transitions enabled if checking logged user info.

--- src/ValdymoSistema/Views/UserViewerController.java
+++ src/ValdymoSistema/Views/UserViewerController.java
@@ -6,20 +6,24 @@
 package ValdymoSistema.Views;
 
 import UserData.CUser;
+import ValdymoSistema.CDataBaseController;
 import ValdymoSistema.CEventHandler;
 import ValdymoSistema.Main;
 import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class UserViewerController implements Initializable
 {
-
+    private CUser user;
     @FXML
     private Label singleUserNameLabel;
     @FXML
@@ -38,53 +42,112 @@ public class UserViewerController implements Initializable
     private ListView<String> projectsOwnerListView;
     @FXML
     private ListView<String> projectsWorkerListView;
+    @FXML
+    private Button editUserInfoButton;
+    @FXML
+    private TextField userNameTextField;
+    @FXML
+    private TextField fullUserNameTextField;
+    @FXML
+    private TextArea userContactInfoTextArea;
+    @FXML
+    private Button confirmEditButton;
 
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
         setSingleUserViewMode(false);
-    }    
-    
+        enableEditorMode(false);
+    }
+
     public void setUser(CUser user)
     {
-        if(user == null)
+        if (user == null)
         {
             return;
         }
         
-        this.userNameLabel.setText(user.getUserName());
-        this.userContactsLabel.setText(user.getUserContacts());
-        this.fullUserNameLabel.setText(user.getUserFullName());
-        
-        setSingleUserViewMode(user.isSingleUser());
-        
+        this.user = user;
+
+        this.userNameLabel.setText(this.user.getUserName());
+        this.userContactsLabel.setText(this.user.getUserContacts());
+        this.fullUserNameLabel.setText(this.user.getUserFullName());
+
+        setSingleUserViewMode(this.user.isSingleUser());
+
         CEventHandler eHandler = Main.getEventHandler();
         
-        Map workingProjects = eHandler.getSavedProjects(user, true, false);
-        Map owningProjects  = eHandler.getSavedProjects(user, false, true);
-        
+        determineCanEditUserInfo(eHandler, this.user);
+
+        Map workingProjects = eHandler.getSavedProjects(this.user, true, false);
+        Map owningProjects = eHandler.getSavedProjects(this.user, false, true);
+
         this.projectsOwnerListView.getItems().clear();
         this.projectsWorkerListView.getItems().clear();
-        
+
         fillListView(workingProjects, this.projectsWorkerListView);
         fillListView(owningProjects, this.projectsOwnerListView);
     }
-    
+
     private void setSingleUserViewMode(boolean enable)
     {
         this.singleUserNameLabel.setVisible(enable);
         this.singleUserContacs.setVisible(enable);
-        
+
         this.companyUserNameLabel.setVisible(!enable);
         this.companyContactLabel.setVisible(!enable);
     }
-    
+
     private void fillListView(Map map, ListView list)
     {
-        for(Object obj : map.keySet())
+        for (Object obj : map.keySet())
         {
             String key = (String) obj;
             list.getItems().add(key);
         }
+    }
+
+    private void enableEditorMode(boolean enable)
+    {
+        //editor mode items
+        this.confirmEditButton.setVisible(enable);
+        this.fullUserNameTextField.setVisible(enable);
+        this.userContactInfoTextArea.setVisible(enable);
+        this.userNameTextField.setVisible(enable);
+
+        //view mode items
+        this.editUserInfoButton.setVisible(!enable);
+        this.fullUserNameLabel.setVisible(!enable);
+        this.userContactsLabel.setVisible(!enable);
+        this.userNameLabel.setVisible(!enable);
+
+        if (enable)
+        {
+            this.fullUserNameTextField.setText(this.fullUserNameLabel.getText());
+            this.userContactInfoTextArea.setText(this.userContactsLabel.getText());
+            this.userNameTextField.setText(this.userNameLabel.getText());
+        }
+    }
+    
+    private void determineCanEditUserInfo(CEventHandler eHandler, CUser user)
+    {
+        CUser loggedUser = eHandler.getCurrentUser();
+        
+        if(user != loggedUser)
+        {
+            this.editUserInfoButton.setVisible(false);
+        }
+    }
+
+    @FXML
+    private void onEditUserInfo(ActionEvent event)
+    {
+        enableEditorMode(true);
+    }
+
+    @FXML
+    private void onConfirmEditUserInfo(ActionEvent event)
+    {
+        enableEditorMode(false);
     }
 }
